5fa1201f04470a388b495b520f7c313ea7039561
Dave <sahajdavework@pm.me>
2024-03-07 00:28:25 +0530

    updated navbar colors

--- js-verse/app/components/navbar.tsx
+++ js-verse/app/components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-5xl mx-auto px-4 py-5">
       <Link href={"/"} className="font-bold text-4xl">
-        <span className="text-slate-600">JS</span>
+        <span className="text-slate-600 dark:text-slate-400">JS</span>
         <span className="text-primary">Verse</span>
       </Link>
       <ModeToggle />
