5beea6a99198bd27da3eb9cf06dd2fdacef59217
Dave <sahajdavework@pm.me>
2024-03-14 00:58:13 +0530

    renamed blog :)

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-5xl mx-auto px-4 py-5 lg:py-10">
       <Link href={"/"} className="font-bold text-4xl">
-        <span className="text-slate-600 dark:text-slate-400">JS</span>
-        <span className="text-primary">Verse</span>
+        <span className="text-slate-600 dark:text-slate-400">The</span>
+        <span className="text-primary">Arbitrary</span>
       </Link>
       <div className="flex flex-nowrap items-center gap-2 lg:gap-10">
         <div className="flex flex-nowrap items-center gap-2">
