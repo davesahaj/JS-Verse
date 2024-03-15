c5488b9fa980fe806e076f5e8e6051a164789c83
Dave <sahajdavework@pm.me>
2024-03-15 14:02:15 +0530

    refactored components

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,13 +5,13 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-5xl mx-auto px-4 py-5 lg:py-10">
-      <Link href={"/"} className="font-bold text-4xl">
+      <Link href={"/"} className="font-bold text-2xl lg:text-4xl">
         <span className="text-slate-600 dark:text-slate-400">The</span>
         <span className="text-primary">Arbitrary</span>
       </Link>
       <div className="flex flex-nowrap items-center gap-2 lg:gap-10">
         <div className="flex flex-nowrap items-center gap-2">
-          <span className="font-medium  text-md lg:text-lg font-lato italic">
+          <span className="font-medium  text-sm lg:text-lg font-lato italic">
             A blog by
           </span>
           <a href="https://sahajdave.com" target="_blank">
