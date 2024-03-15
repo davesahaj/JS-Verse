07e7de683e7632693eeccd3b1e84b45adff7392d
Dave <sahajdavework@pm.me>
2024-03-15 15:34:59 +0530

    added background gradients

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(lato.variable)}>
+      <body className={cn(lato.variable, "")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -35,6 +35,8 @@ export default function RootLayout({
         >
           <Navbar />
           <main className="max-w-6xl mx-auto px-4 font-lato">{children}</main>
+
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         </ThemeProvider>
       </body>
     </html>
